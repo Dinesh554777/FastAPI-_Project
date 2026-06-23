855a8d897331913bf1c122a0ac52897aa6dcb0c6
DINESH <dinesh3212001@gmail.com>
2026-06-23 22:47:45 +0530

    Add student retrieval endpoints and optional query parameters

--- tutorials/myapi.py
+++ tutorials/myapi.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi import Path
+from typing import Optional
 
 app = FastAPI()
 students={
@@ -12,8 +13,18 @@ students={
 def index():
     return {"name": "first Data"}
 
-@app.get("/get-students/{student_id}")
+@app.get("/get-students/{student_id}") #path parameter
 def get_students(student_id: int= Path(description="The ID of the student you want to view", gt=0
                 )
     ):
     return students[student_id]
+
+@app.get("/get-by-name/{student_id}") #path and query parameter
+def get_student(*, student_id : int,    # keyword-only arguments start here
+                name : Optional[str] = None, # keyword-only arguments start here
+                test : int):  # required integer parameter
+    for student_id in students:
+        if students[student_id]["name"] == name:
+            return students[student_id]
+    return {"Data": "Not Found"}  
+
